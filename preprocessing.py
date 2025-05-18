9eb5b56335a4f8e288755ec701635e41a8936dcd
banchan316 <cksehfdl0316@gmail.com>
2025-05-18 16:41:54 +0900

    V5: drop 찐

--- preprocessing.py
+++ preprocessing.py
@@ -3,12 +3,13 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
-def preprocessing(df):
-    #원본 데이터
+def preprocessing(df,target_name):
+
+    """원본 데이터
     print("1. 원본 데이터 정보\n")
-    #print(f"데이터 형태: {df.shape}")
-    #df.info()
-    #print(df.head())
+    print(f"데이터 형태: {df.shape}")
+    df.info()
+    print(df.head())"""
 
     #feature engineering
 
@@ -16,15 +17,22 @@ def preprocessing(df):
     df['Start_Time'] = pd.to_datetime(df['Start_Time'], format='mixed') #뒤에 00000때메 mixed 사용
     df['End_Time'] = pd.to_datetime(df['End_Time'], format='mixed')
     df['Duration_Minutes'] = (df['End_Time'] - df['Start_Time']).dt.total_seconds() / 60 #초 단위 float으로 바꾸기
-    df = df[df['Duration_Minutes'] >= 0] # 음수 Duration 제거
-
-    y_severity = df['Severity'].copy() #Severity 타겟
-    y_duration = df['Duration_Minutes'].copy() #Duration 타겟
+    df = df[df['Duration_Minutes'] >= 0] #음수 Duration 제거
+    df = df[df['Duration_Minutes'] <= 1440 * 7]  #7일보다 긴 이상치 제거
 
     print("시간 관련 피처 생성")
     df['Start_Hour'] = df['Start_Time'].dt.hour
     df['Start_Month'] = df['Start_Time'].dt.month
     df['Start_DayOfWeek'] = df['Start_Time'].dt.dayofweek #요일 0 ~ 6정수로 바꾸기
+
+    #타겟 값 설정 
+    if target_name == 'Severity':
+        df['Severity_binary'] = np.where(df['Severity'] <= 2, 0, 1) #1,2 / 3,4로 묶기 
+        y = df['Severity_binary'].astype(int).copy()
+    elif target_name == 'Duration_Minutes':
+        y = df['Duration_Minutes'].copy()#Duration_Minutes를 y로 사용
+    else:
+        print("에러요.")
 
     cat_names = [
     'Source', 'Severity', 'City', 'County', 'State', 'Country',
@@ -33,7 +41,7 @@ def preprocessing(df):
     ]
 
     for col in cat_names:
-        print(col, df[col].unique().size) #country 단일 클래스라 삭제 
+        print(col, df[col].unique().size)#country 단일 클래스라 삭제 
 
     features_to_drop = [
         'ID', 'Source', 'Start_Time', 'End_Time',
@@ -46,6 +54,20 @@ def preprocessing(df):
         'Amenity', 'Bump', 'Give_way', 'No_Exit', 'Railway', 'Roundabout', 'Station', 'Stop', 'Traffic_Calming', 'Turning_Loop', #inspection 결과 너무 편향됨
         'Civil_Twilight', 'Nautical_Twilight', 'Astronomical_Twilight' # Twilight 컬럼들 (Sunrise_Sunset과 유사하며, 카디널리티 줄이기 위해 하나만 남김)
     ]
+    if target_name == 'Severity':
+        features_to_drop.extend(['Severity','Severity_binary'])
+    elif target_name == 'Duration_Minutes':
+        features_to_drop.append('Duration_Minutes')
 
+    df.drop(columns=features_to_drop, inplace=True, errors='ignore')
+    print(f"제거된 피처: {features_to_drop}")
+
+    x = df.copy()
+
+    return x,y
+    
 df = pd.read_csv("US_Accidents_March23_sampled_500k.csv")
-preprocessing(df)
+x,y = preprocessing(df, 'Severity') #Severity 또는 Duration_Minutes 입력 
+
+print(x.columns)  
+print(y.value_counts())
