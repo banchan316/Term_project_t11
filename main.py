6b0c1e7dca27181e185d333ba83f71272a72559e
banchan316 <cksehfdl0316@gmail.com>
2025-05-23 16:32:28 +0900

    전처리까지 완료용

--- main.py
+++ main.py
@@ -1,25 +1,44 @@
 import numpy as np
 import pandas as pd
-from preprocessing import preprocessing
-from handle_missing_value import handle_missing_value
+import preprocessing as pp 
+import feature_scaling as fs
 
 def main():
-    # 데이터 로드
     df = pd.read_csv("US_Accidents_March23_sampled_500k.csv")
-    print(f"데이터 로드 완료: {df.shape}")
-    
-    # 기본 전처리 (preprocessing.py 활용)
-    X, y = preprocessing(df, target_name='Severity')
-    print(f"기본 전처리 완료: {X.shape}")
-    
-    # 결측치 처리 (handle_missing_value.py 활용)
-    X_imputed = handle_missing_value(X, verbose=True)
+    # column drop 및 타겟 ,변수 생성 
+    x,y = pp.drop(df, 'Severity') #Severity 또는 Duration_Minutes 입력 
+    x.to_csv("US_Accidents_dropped.csv", index=False)
+
+    #결측치 처리 
+    X_imputed, missing_indices = pp.handle_missing_value(x, verbose=True)
     print(f"결측치 처리 완료: {X_imputed.shape}")
+    missing_count = X_imputed.isnull().sum().sum()
+    print(f"남아있는 결측치 수: {missing_count}")
     
-    # 처리된 데이터 저장
-    X_imputed.to_csv("US_Accidents_missing_values_handled.csv", index=False)
-    print("처리된 데이터를 'US_Accidents_missing_values_handled.csv'로 저장")
+    #원 핫 인코딩 
+    X_encoded = pp.one_hot_encoding(X_imputed)
+    print(f" One_Hot_Encoding 완료: {X_encoded.shape}")
+    X_encoded.to_csv("US_Accidents_encoded.csv", index=False)
     
+    #특성 분석  
+    numeric_stats = fs.analyze_numeric_features(X_encoded, visualize = False )
+    
+    # 5. 정규화 (feature_scaling.py)
+    print("\n5. 정규화(StandardScaler) 적용")
+    X_scaled, scaler = fs.scale_features(X_encoded, method='standard', verbose=True)
+    print(f"정규화 완료: {X_scaled.shape}")
+    
+    # 6. 다른 스케일링 방법 테스트 (MinMaxScaler)
+    print("\n6. 정규화(MinMaxScaler) 적용")
+    X_minmax, minmax_scaler = fs.scale_features(X_encoded, method='minmax', verbose=True)
+    
+    # 7. 다른 스케일링 방법 테스트 (RobustScaler)
+    print("\n7. 정규화(RobustScaler) 적용")
+    X_robust, robust_scaler = fs.scale_features(X_encoded, method='robust', verbose=True)
+
+    fs.visualize_scaling_effect(X_encoded,X_scaled)
+    fs.visualize_scaling_effect(X_encoded,X_minmax)
+    fs.visualize_scaling_effect(X_encoded,X_robust)
     return X_imputed, y
 
 if __name__ == "__main__":
